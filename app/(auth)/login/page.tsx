92948b0e815ba41c6edbb3389302eda9c09284b9
sunil parmar <sunil04ce@gmail.com>
2024-02-03 20:40:52 +0530

    ui changes in login, signup, added navbar

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import LoginCard from "./login-card";
 
-const LoginPage = () => {
-  return <div>LoginPage</div>;
-};
-
-export default LoginPage;
+export default async function Login() {
+  return (
+    <>
+      <h1 className="text-2xl font-bold text-center mb-4">
+        Log in to SnapNext
+      </h1>
+      <LoginCard />
+    </>
+  );
+}
--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import SignupCard from "./signup-card";
 
-const SignUpPage = () => {
-  return <div>SignUpPage</div>;
-};
-
-export default SignUpPage;
+export default async function Signup() {
+  return (
+    <>
+      <h1 className="text-2xl font-bold text-center mb-4">
+        Sign up to SnapNext
+      </h1>
+      <SignupCard />
+    </>
+  );
+}
